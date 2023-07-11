ed9a75e309965fb605ff2cc0f4235ab14ac9c803
umair-sagedigital <umair@sagedigital.com>
2023-07-11 20:32:05 +0500

    Initial commit

---
+++ app/models/quote.rb
@@ -0,0 +1,8 @@
+class Quote < ApplicationRecord
+    belongs_to :company
+    has_many :line_item_dates, dependent: :destroy
+
+    validates :name, presence: true
+
+    broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
+end
